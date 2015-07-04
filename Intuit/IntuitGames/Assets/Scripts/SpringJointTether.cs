f6f9c7d794e28a23b7c48ce0ecad196d123dfcd5
Aiken Parker <aikenparker95@gmail.com>
2015-07-04 18:38:19 +1000

    Testing with the spring joint:
    • Added two capsule characters with rigid bodies and a spring joint.
    (Can be tested if you enter play mode and move them around in the scene view)
    
    • Added a basic tether object that scales, re-positions and rotates depending on the capsule characters.

---
+++ Intuit/IntuitGames/Assets/Scripts/SpringJointTether.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Simply modifies the transform for this object so that it stretches between to given objects.
+/// </summary>
+[ExecuteInEditMode]
+public class SpringJointTether : MonoBehaviour
+{
+    // Component references
+    public Transform ObjectA;
+    public Transform ObjectB;
+
+    private void Awake()
+    {
+        // Check for reference validity
+        if(!ObjectA || !ObjectB)
+        {
+            Debug.LogWarning("Both objects must be specified.");
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if (!ObjectA || !ObjectB)
+            return;
+
+        // Determine the scale of the tether
+        float DistanceBetweenCharacters = Vector3.Distance(ObjectA.position, ObjectB.position);
+        Vector3 ScaleVec = new Vector3(transform.localScale.x, transform.localScale.y, DistanceBetweenCharacters);
+        transform.localScale = ScaleVec;
+
+        // Determine the position of the tether (half-way between the characters)
+        Vector3 PositionVec = ObjectB.position + (ObjectA.position - ObjectB.position) * 0.5f;
+        transform.localPosition = PositionVec;
+
+        // Determine the rotation
+        transform.localRotation = Quaternion.LookRotation(ObjectA.position - ObjectB.position);
+    }
+}
