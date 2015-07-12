331a540f5012e6d80eb5587df9b8d44e814d5abe
Aiken Parker <aikenparker95@gmail.com>
2015-07-12 22:44:41 +1000

    Added a smooth camera follow that is currently attached to the player tether.
    Extended the level scene into an "L" shape to better show off our intended level structure. Also added a couple of temporary cubes so I could better see how the characters were moving in the environment. (New material)
    
    (Have been working on the multi-joint tether for awhile now, but I the results are very strange and difficult to manage, I'll keep trying to see what I can do)

---
+++ Intuit/IntuitGames/Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Simply gets the camera to follow a target transform.
+/// </summary>
+[RequireComponent(typeof(Camera))]
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float Distance = 5;
+    public float SmoothTime = 0.3f;
+    public bool OverrideOffset = false;
+    public Vector3 OverridenOffsetDirection = new Vector3(0, 0.5f, -1);
+
+    private Vector3 NewOffset;
+    private Vector3 InitialOffsetDirection;
+    private Vector3 Velocity = Vector3.zero;
+
+    void Awake()
+    {
+        InitialOffsetDirection = (Target.transform.position - transform.position).normalized;
+    }
+
+    void Update()
+    {
+        NewOffset = OverrideOffset ?
+            Target.transform.position + (OverridenOffsetDirection.normalized * Distance) :
+            Target.transform.position + (-InitialOffsetDirection * Distance);
+
+        transform.position = Vector3.SmoothDamp(transform.position, NewOffset, ref Velocity, SmoothTime);
+    }
+}
