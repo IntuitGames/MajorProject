ba7224d6bbdeb6c4d72d1f8faf8beb470894ba84
Lachlan Brown <lachlan.brown8@live.com.au>
2015-12-03 02:32:43 +1100

    Very rough death transitions for Totem Enemy.
    • If not hit correctly, will cause one of many weird behaviors (happens way more often than not)
    • When dying, the enemy switches in a new kinda ragdoll model (still need to add transparent materials for the purpose of fading the body parts out)
    • Tried to workaround destroying / disabling the enemy when still considered a collision but not working fully just yet.

---
+++ Intuit/IntuitGames/Assets/Scripts/Enemy/WeakSpot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+
+public class WeakSpot : Trigger
+{
+    public EnemyBase ownerEnemy;
+
+    protected override bool canBeTriggered
+    {
+        get
+        {
+            return !TetherManager.TetherManager.disconnected; //&& GameManager.PlayerManager.character1.isDashing && GameManager.PlayerManager.character2.isDashing; // When it works nicely without dashing, 
+        }
+    }
+
+    protected override void OnTrigger(GameObject triggerObject)
+    {
+        ownerEnemy.OnDeath();
+    }
+}
